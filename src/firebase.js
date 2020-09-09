983fb5f8bef918efeece0ba72b06c98e713eeaf9
Sriram Rakshith <sriramrakshith@gmail.com>
2020-09-09 00:47:43 -0400

    First-clone

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC_VX6Ov94xhQA-d96dcZAXCHIx1EaGXjQ",
+    authDomain: "clone-1-e14e1.firebaseapp.com",
+    databaseURL: "https://clone-1-e14e1.firebaseio.com",
+    projectId: "clone-1-e14e1",
+    storageBucket: "clone-1-e14e1.appspot.com",
+    messagingSenderId: "947761097804",
+    appId: "1:947761097804:web:f5cf8a8c9a91aea2dd18e9",
+    measurementId: "G-PVTJH7EGEQ"
+  };
